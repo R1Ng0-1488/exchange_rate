439a90d4188a173dc8cd42bc74b8d9138628909b
R1Ng0-1488 <t1m.sadist@gmail.com>
2025-03-13 13:54:22 +0500

    second commit

--- main/views.py
+++ main/views.py
@@ -13,7 +13,8 @@ class GetUsdRateView(APIView):
 	def get(self, request, format=None):
 		try:
 			r = requests.get(f'https://{settings.URL}/v6/{settings.KEY}/latest/USD')
-			rub = r.json()['conversion_rates']['RUB']
+			data = r.json()
+			rub = data['conversion_rates']['RUB']
 			USDRate.objects.create(usd=f'{rub} рублей')
 			models = USDRate.objects.order_by('-created')[:10]
 			serializer = USDRateSerializer(models, many=True)
